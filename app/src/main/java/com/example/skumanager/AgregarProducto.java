f78b3dc7f82758dc52551406327e7239a5c0e4dc
Marco Guerrero <ing._guerrero@hotmail.com>
2019-12-02 16:26:29 -0600

    Se agrego la funcion de codigo de barras en activity de productos

--- app/src/main/java/com/example/skumanager/AgregarProducto.java
+++ app/src/main/java/com/example/skumanager/AgregarProducto.java
@@ -1,7 +1,9 @@
 package com.example.skumanager;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +13,15 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.skumanager.utilidades.Utilidades;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class AgregarProducto extends AppCompatActivity {
 
-    public EditText ID,CB,DSC,UM,PU;
+    public EditText ID, CB, DSC, UM, PU;
     public Button AgregarPr;
     public ImageView Back;
+    public ImageView lector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +35,19 @@ public class AgregarProducto extends AppCompatActivity {
         PU = (EditText) findViewById(R.id.txtUM);
         AgregarPr = (Button) findViewById(R.id.btnAgregarPr);
         Back = (ImageView) findViewById(R.id.imgBack);
+        lector = (ImageView) findViewById(R.id.imageView);
+
+        lector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                escaner();
+            }
+        });
+
     }
 
-    public void onClick(View view){
+
+    public void onClick(View view) {
         registrarProductos();
     }
 
@@ -56,14 +71,47 @@ public class AgregarProducto extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Producto: " + producto, Toast.LENGTH_SHORT).show();
 
                     finish();
-                }else{
+                } else {
                     Toast.makeText(getApplicationContext(), "Ingrese un identificador", Toast.LENGTH_SHORT).show();
                 }
-            }else{
+            } else {
                 Toast.makeText(getApplicationContext(), "Ingrese un código de barras", Toast.LENGTH_SHORT).show();
             }
-        }else{
+        } else {
             Toast.makeText(getApplicationContext(), "Ingrese una descripción", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void escaner() {
+        IntentIntegrator intent = new IntentIntegrator(this);
+        intent.setDesiredBarcodeFormats(IntentIntegrator.PRODUCT_CODE_TYPES);
+
+        intent.setPrompt("Escanear Codigo");
+        intent.setCameraId(0);
+        intent.setBeepEnabled(false);
+        intent.setBarcodeImageEnabled(false);
+        intent.initiateScan();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null) {
+
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cacelaste el escaneo", Toast.LENGTH_LONG).show();
+
+            } else {
+                CB.setText(result.getContents().toString());
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
+    }
 }
+
